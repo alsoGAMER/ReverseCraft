77f1a84500fdb59f3ab08918015b5a7875296fd4
alsoGAMER <35269770+alsoGAMER@users.noreply.github.com>
2021-08-03 13:51:17 +0200

    made the plugin functional

--- src/main/java/io/github/ann0y1nghacker/plugin/Plugin.java
+++ src/main/java/io/github/ann0y1nghacker/plugin/Plugin.java
@@ -1,40 +1,18 @@
 package io.github.ann0y1nghacker.plugin;
 
-import io.github.ann0y1nghacker.plugin.commands.test;
-import io.github.ann0y1nghacker.plugin.events.OnPlayerChat;
-import io.github.ann0y1nghacker.plugin.events.OnPlayerJoin;
-import io.github.ann0y1nghacker.plugin.events.OnPlayerLeave;
 import io.github.ann0y1nghacker.plugin.modules.recipes;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
 
 public final class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
         new recipes(this);
-
-        System.out.println(" \n" +
-                "/* ======================================================= */\n" +
-                "/*                                                         */\n" +
-                "/*       © Copyright 2020 - ANN0Y1NGHACKER's Plugins       */\n" +
-                "/*                                                         */\n" +
-                "/* ======================================================= */\n"
-        );
-
-//        Objects.requireNonNull(getCommand("test")).setExecutor(new test());
-
-        getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
-        getServer().getPluginManager().registerEvents(new OnPlayerLeave(), this);
-        getServer().getPluginManager().registerEvents(new OnPlayerChat(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-
     }
 }
--- src/main/java/io/github/ann0y1nghacker/plugin/modules/recipes.java
+++ src/main/java/io/github/ann0y1nghacker/plugin/modules/recipes.java
@@ -13,50 +13,85 @@ public class recipes {
     public recipes(Plugin plugin) {
         this.plugin = plugin;
         Bukkit.addRecipe(oakSlabsToBlocks());
+        Bukkit.addRecipe(spruceSlabsToBlocks());
+        Bukkit.addRecipe(birchSlabsToBlocks());
+        Bukkit.addRecipe(jungleSlabsToBlocks());
+        Bukkit.addRecipe(acaciaSlabsToBlocks());
+        Bukkit.addRecipe(darkoakSlabsToBlocks());
+        Bukkit.addRecipe(crimsonSlabsToBlocks());
+        Bukkit.addRecipe(warpedSlabsToBlocks());
+        Bukkit.addRecipe(stoneSlabsToBlocks());
     }
 
     public ShapelessRecipe oakSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.OAK_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "oak_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.OAK_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.OAK_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "oak_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.OAK_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe spruceSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.SPRUCE_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "spruce_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.SPRUCE_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.SPRUCE_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "spruce_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.SPRUCE_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe birchSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.BIRCH_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "birch_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.BIRCH_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.BIRCH_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "birch_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.BIRCH_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe jungleSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.JUNGLE_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "jungle_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.JUNGLE_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.JUNGLE_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "jungle_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.JUNGLE_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe acaciaSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.ACACIA_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "acacia_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.ACACIA_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.ACACIA_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "acacia_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.ACACIA_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe darkoakSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.DARK_OAK_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "dark_oak_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.DARK_OAK_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.DARK_OAK_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "dark_oak_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.DARK_OAK_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe crimsonSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.CRIMSON_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "crimson_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.CRIMSON_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.CRIMSON_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "crimson_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.CRIMSON_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe warpedSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.WARPED_PLANKS); NamespacedKey key = new NamespacedKey(plugin, "warped_planks"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.WARPED_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.WARPED_PLANKS);
+        NamespacedKey key = new NamespacedKey(plugin, "warped_planks");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.WARPED_SLAB);
+        return recipe;
     }
 
     public ShapelessRecipe stoneSlabsToBlocks() {
-        ItemStack item = new ItemStack(Material.STONE); NamespacedKey key = new NamespacedKey(plugin, "stone"); ShapelessRecipe recipe = new ShapelessRecipe(key, item);
-        recipe.addIngredient(2, Material.STONE_SLAB); return recipe;
+        ItemStack item = new ItemStack(Material.STONE);
+        NamespacedKey key = new NamespacedKey(plugin, "stone");
+        ShapelessRecipe recipe = new ShapelessRecipe(key, item);
+        recipe.addIngredient(2, Material.STONE_SLAB);
+        return recipe;
     }
 }
